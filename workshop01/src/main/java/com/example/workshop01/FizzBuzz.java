1ad02390f0114d2cdd4209efe6c7b59d5c718c32
up1 <somkiat.p@gmail.com>
2021-02-22 15:23:35 +0700

    Done in first solution

--- workshop01/src/main/java/com/example/workshop01/FizzBuzz.java
+++ workshop01/src/main/java/com/example/workshop01/FizzBuzz.java
@@ -2,6 +2,15 @@ package com.example.workshop01;
 
 public class FizzBuzz {
     public String say(int number) {
+        if(number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }
+        if(number % 5 == 0) {
+            return "Buzz";
+        }
+        if(number % 3 == 0) {
+            return "Fizz";
+        }
         return "" + number;
     }
 }
--- workshop01/src/test/java/com/example/workshop01/FizzBuzzTest.java
+++ workshop01/src/test/java/com/example/workshop01/FizzBuzzTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
     @DisplayName("นับ 1 ได้ 1")
     public void case_1() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String actualResult = fizzBuzz.say(1);
         assertEquals("1", actualResult);
     }
@@ -18,8 +18,77 @@ public class FizzBuzzTest {
     @Test
     @DisplayName("นับ 2 ได้ 2")
     public void case_2() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String actualResult = fizzBuzz.say(2);
         assertEquals("2", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 3 ได้ Fizz")
+    public void case_3() {
+        String actualResult = fizzBuzz.say(3);
+        assertEquals("Fizz", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 4 ได้ 4")
+    public void case_4() {
+        String actualResult = fizzBuzz.say(4);
+        assertEquals("4", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 5 ได้ Buzz")
+    public void case_5() {
+        String actualResult = fizzBuzz.say(5);
+        assertEquals("Buzz", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 6 ได้ Fizz")
+    public void case_6() {
+        String actualResult = fizzBuzz.say(6);
+        assertEquals("Fizz", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 7 ได้ 7")
+    public void case_7() {
+        String actualResult = fizzBuzz.say(7);
+        assertEquals("7", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 8 ได้ 8")
+    public void case_8() {
+        String actualResult = fizzBuzz.say(8);
+        assertEquals("8", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 9 ได้ Fizz")
+    public void case_9() {
+        String actualResult = fizzBuzz.say(9);
+        assertEquals("Fizz", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 10 ได้ Buzz")
+    public void case_10() {
+        String actualResult = fizzBuzz.say(10);
+        assertEquals("Buzz", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 11 ได้ 11")
+    public void case_11() {
+        String actualResult = fizzBuzz.say(11);
+        assertEquals("11", actualResult);
+    }
+
+    @Test
+    @DisplayName("นับ 15 ได้ FizzBuzz")
+    public void case_15() {
+        String actualResult = fizzBuzz.say(15);
+        assertEquals("FizzBuzz", actualResult);
     }
 }
