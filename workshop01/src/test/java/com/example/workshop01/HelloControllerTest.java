03ff92401480bb24bacd7aba5589399340b7bc7c
up1 <somkiat.p@gmail.com>
2021-02-23 14:15:16 +0700

    First spring boot test

---
+++ workshop01/src/test/java/com/example/workshop01/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.workshop01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void success_hello() {
+        Message result =
+                testRestTemplate.getForObject("/hello", Message.class);
+        assertEquals("Hello world", result.getMessage());
+    }
+
+}
