88f876824d75985a8388aab23e8eb99fd659c8f4
up1 <somkiat.p@gmail.com>
2021-02-23 15:55:44 +0700

    Working with get all users

--- workshop01/src/main/java/com/example/workshop01/user/UserController.java
+++ workshop01/src/main/java/com/example/workshop01/user/UserController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 @RestController
 public class UserController {
 
@@ -12,5 +16,19 @@ public class UserController {
         UserResponse userResponse = new UserResponse(id, "somkiat", 40);
         return userResponse;
     }
+
+    @GetMapping("/users")
+    public List<UserResponse> getAlll() {
+        UserResponse user1 = new UserResponse(1, "somkiat", 40);
+        UserResponse user2 = new UserResponse(2, "somkiat", 40);
+        UserResponse user3 = new UserResponse(3, "somkiat", 40);
+        List<UserResponse> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        return users;
+    }
+
+
 
 }
--- workshop01/src/test/java/com/example/workshop01/user/UserControllerTest.java
+++ workshop01/src/test/java/com/example/workshop01/user/UserControllerTest.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -21,6 +25,16 @@ public class UserControllerTest {
         assertEquals(1, result.getId());
         assertEquals("somkiat", result.getName());
         assertEquals(40, result.getAge());
+    }
+
+    @Test
+    public void get_all_user_with_size_3() {
+        List<LinkedHashMap> result
+                = restTemplate.getForObject("/users", List.class);
+        assertEquals(3, result.size());
+        Map mapResult = result.get(2);
+        assertEquals(3, mapResult.get("id"));
+        assertEquals("somkiat", mapResult.get("name"));
     }
 
 }
