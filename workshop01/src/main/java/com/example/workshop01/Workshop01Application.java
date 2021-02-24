11155a5b0ed23abdf9d8e57a7cb2d3c125f0c7db
up1 <somkiat.p@gmail.com>
2021-02-24 16:55:23 +0700

    Working with gateway

--- workshop01/src/main/java/com/example/workshop01/Workshop01Application.java
+++ workshop01/src/main/java/com/example/workshop01/Workshop01Application.java
@@ -5,7 +5,10 @@ import com.example.workshop01.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 
@@ -21,6 +24,11 @@ public class Workshop01Application {
 	public void initialData() {
 		MyUser demoUser = new MyUser(123, "demo user", 100);
 		userRepository.save(demoUser);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplateBuilder().build();
 	}
 
 	public static void main(String[] args) {
