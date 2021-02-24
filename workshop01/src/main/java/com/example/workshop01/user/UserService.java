f226158cfec1b3e1071539a7d7b2031083707ce3
up1 <somkiat.p@gmail.com>
2021-02-24 12:03:40 +0700

    Mocking with mockito

--- workshop01/src/main/java/com/example/workshop01/user/UserService.java
+++ workshop01/src/main/java/com/example/workshop01/user/UserService.java
@@ -1,13 +1,26 @@
 package com.example.workshop01.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public UserResponse findById(int id) {
-        if (id >= 1) {
+        Optional<MyUser> result = userRepository.findById(id);
+        if (result.isPresent()) {
             // Found
-            UserResponse userResponse = new UserResponse(id, "somkiat", 40);
+            UserResponse userResponse = new UserResponse(
+                    result.get().getId(), result.get().getName(), result.get().getAge());
             return userResponse;
         } else {
             // Not found
--- workshop01/src/test/java/com/example/workshop01/user/UserServiceTest.java
+++ workshop01/src/test/java/com/example/workshop01/user/UserServiceTest.java
@@ -1,18 +1,37 @@
 package com.example.workshop01.user;
 
-import org.junit.jupiter.api.Assertions;
+import static org.mockito.BDDMockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void user_found_with_id_1() {
+        MyUser somkiat = new MyUser(1, "somkiat", 40);
+        when(userRepository.findById(1))
+                .thenReturn(Optional.of(somkiat));
+        UserService userService = new UserService(userRepository);
+        UserResponse userResponse = userService.findById(1);
+        assertEquals(1, userResponse.getId());
+    }
+
     @Test
     public void user_not_found_with_id_0() {
-        UserService userService = new UserService();
+        when(userRepository.findById(0))
+                .thenReturn(Optional.empty());
+        UserService userService = new UserService(userRepository);
         Exception exception = assertThrows(UserNotFoundException.class, () -> {
             userService.findById(0);
         });
